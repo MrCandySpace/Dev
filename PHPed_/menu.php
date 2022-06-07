94ce62f7c3f4063ef49976d112393bb38fa7642d
MrCandySpace <100863652+MrCandySpace@users.noreply.github.com>
2022-06-07 17:09:58 +0200

    V1

---
+++ PHPed_/menu.php
@@ -0,0 +1,10 @@
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="plus-d-info.php">En savoir plus</a></li>
+            </ul>
+        </nav>
+    </header>
