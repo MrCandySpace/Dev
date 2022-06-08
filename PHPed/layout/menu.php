7fc69f6f0e96fa5144e412e6078c8efa5f410fea
MrCandySpace <100863652+MrCandySpace@users.noreply.github.com>
2022-06-08 10:26:52 +0200

    V1.0

---
+++ PHPed/layout/menu.php
@@ -0,0 +1,11 @@
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="index.php?page=contact">Contact</a></li>
+                <li><a href="index.php?page=esp">En savoir plus</a></li>
+                <li><a href="index.php?page=connexion">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
